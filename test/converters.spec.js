608412dac651737c0f0be888424afe423d12f522
Daniel Roux <roux@epsitec.ch>
2019-01-18 17:00:18 +0100

    WIP: Add tests for percent.

--- test/converters.spec.js
+++ test/converters.spec.js
@@ -212,6 +212,10 @@ describe('Converter number', function() {
   it('#Test parseEdited', function() {
     let result;
 
+    result = NumberConverters.parseEdited('');
+    assert.equal(result.value, null);
+    assert.equal(result.error, null);
+
     result = NumberConverters.parseEdited('123');
     assert.equal(result.value, '123');
     assert.equal(result.error, null);
@@ -234,6 +238,10 @@ describe('Converter number', function() {
 
     result = NumberConverters.parseEdited('123.800');
     assert.equal(result.value, '123.8');
+    assert.equal(result.error, null);
+
+    result = NumberConverters.parseEdited('123.456789');
+    assert.equal(result.value, '123.456789');
     assert.equal(result.error, null);
 
     result = NumberConverters.parseEdited('.123');
@@ -280,6 +288,10 @@ describe('Converter price', function() {
   it('#Test parseEdited', function() {
     let result;
 
+    result = PriceConverters.parseEdited('');
+    assert.equal(result.value, null);
+    assert.equal(result.error, null);
+
     result = PriceConverters.parseEdited('123.00');
     assert.equal(result.value, '123');
     assert.equal(result.error, null);
@@ -307,6 +319,60 @@ describe('Converter price', function() {
     assert.ok(PriceConverters.check('123.456'));
     assert.ok(PriceConverters.check('-123'));
     assert.ok(!PriceConverters.check('-1x3'));
+  });
+});
+
+//-----------------------------------------------------------------------------
+// percent
+//-----------------------------------------------------------------------------
+describe('Converter percent', function() {
+  it('#Test parseEdited', function() {
+    let result;
+
+    result = PercentConverters.parseEdited('');
+    assert.equal(result.value, null);
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('45%');
+    assert.equal(result.value, '0.45');
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('45.7%');
+    assert.equal(result.value, '0.457');
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('.2%');
+    assert.equal(result.value, '0.002');
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('.002%');
+    assert.equal(result.value, '0.00002');
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('0.45');
+    assert.equal(result.value, '0.45');
+    assert.equal(result.error, null);
+
+    result = PercentConverters.parseEdited('4%2');
+    assert.equal(result.value, null);
+    assert.ok(result.error);
+
+    result = PercentConverters.parseEdited('abc');
+    assert.equal(result.value, null);
+    assert.ok(result.error);
+  });
+
+  it('#Test getDisplayed', function() {
+    assert.equal(PercentConverters.getDisplayed('0.12'), '12%');
+    assert.equal(PercentConverters.getDisplayed('0.12', 5), '12%');
+    assert.equal(PercentConverters.getDisplayed('0.1234999', 2), '12.35%');
+    assert.equal(PercentConverters.getDisplayed('0.123'), '12.3%');
+  });
+
+  it('#Test check', function() {
+    assert.ok(PercentConverters.check('123'));
+    assert.ok(PercentConverters.check('0.123'));
+    assert.ok(!PercentConverters.check('-0.05'));
   });
 });
 
