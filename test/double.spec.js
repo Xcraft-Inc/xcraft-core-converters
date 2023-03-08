832c9ea41f311d314255b90a90f913bfa9a13700
Samuel Loup <loup@epsitec.ch>
2023-03-08 15:29:35 +0100

    Rework 'double' converters type
    This "forgotten" type was not applicable with
    our usage:
    
    - remove number of decimals limitations
    - remove min/max canonical value
    - keep canonical value compatibility with original impl.

--- test/double.spec.js
+++ test/double.spec.js
@@ -34,7 +34,15 @@ describe('Converter double', function () {
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('123.00');
-    assert.strictEqual(result.value, '123.00');
+    assert.strictEqual(result.value, '123');
+    assert.strictEqual(result.error, null);
+
+    result = DoubleConverters.parseEdited('0.0123456789123456');
+    assert.strictEqual(result.value, '0.0123456789123456');
+    assert.strictEqual(result.error, null);
+
+    result = DoubleConverters.parseEdited('0.0123456789123456991');
+    assert.strictEqual(result.value, '0.0123456789123456991');
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('123.810');
@@ -42,15 +50,19 @@ describe('Converter double', function () {
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('123.456789');
-    assert.strictEqual(result.value, '123.45');
+    assert.strictEqual(result.value, '123.456789');
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('.123');
-    assert.strictEqual(result.value, '.12');
+    assert.strictEqual(result.value, '0.123');
+    assert.strictEqual(result.error, null);
+
+    result = DoubleConverters.parseEdited('-.123');
+    assert.strictEqual(result.value, '-0.123');
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('0.123');
-    assert.strictEqual(result.value, '0.12');
+    assert.strictEqual(result.value, '0.123');
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited(' 12');
@@ -65,8 +77,12 @@ describe('Converter double', function () {
     assert.strictEqual(result.value, '12');
     assert.strictEqual(result.error, null);
 
+    result = DoubleConverters.parseEdited('12 ');
+    assert.strictEqual(result.value, '12');
+    assert.strictEqual(result.error, null);
+
     result = DoubleConverters.parseEdited("1'000.00");
-    assert.strictEqual(result.value, '1000.00');
+    assert.strictEqual(result.value, '1000');
     assert.strictEqual(result.error, null);
 
     result = DoubleConverters.parseEdited('12a34');
@@ -100,33 +116,18 @@ describe('Converter double', function () {
     result = DoubleConverters.parseEdited('12.3.4');
     assert.strictEqual(result.value, null);
     assert.notStrictEqual(result.error, null);
-
-    result = DoubleConverters.parseEdited('49', '50', '100');
-    assert.strictEqual(result.value, '50');
-    assert.ok(result.error);
-
-    result = DoubleConverters.parseEdited('101', '50', '100');
-    assert.strictEqual(result.value, '100');
-    assert.ok(result.error);
-
-    result = DoubleConverters.parseEdited('-101', '-100', '-50');
-    assert.strictEqual(result.value, '-100');
-    assert.ok(result.error);
-
-    result = DoubleConverters.parseEdited('-49', '-100', '-50');
-    assert.strictEqual(result.value, '-50');
-    assert.ok(result.error);
   });
 
   // prettier-ignore
   it('#Test getDisplayed', function() {
-    assert.strictEqual(DoubleConverters.getDisplayed(null       ), null);
-    assert.strictEqual(DoubleConverters.getDisplayed('1234'     ), "1'234.00");
-    assert.strictEqual(DoubleConverters.getDisplayed('-1234'    ), "-1'234.00");
-    assert.strictEqual(DoubleConverters.getDisplayed('.12'      ), '0.12');
-    assert.strictEqual(DoubleConverters.getDisplayed('.12456'),    '0.12');
-    assert.strictEqual(DoubleConverters.getDisplayed(1234       ), "1'234.00");
-    assert.strictEqual(DoubleConverters.getDisplayed(0          ), "0.00");
+    assert.strictEqual(DoubleConverters.getDisplayed(null), null);
+    assert.strictEqual(DoubleConverters.getDisplayed('1234'), "1'234");
+    assert.strictEqual(DoubleConverters.getDisplayed('-1234'), "-1'234");
+    assert.strictEqual(DoubleConverters.getDisplayed('.12'), '0.12');
+    assert.strictEqual(DoubleConverters.getDisplayed('.12456'),    '0.12456');
+    assert.strictEqual(DoubleConverters.getDisplayed(1234), "1'234");
+    assert.strictEqual(DoubleConverters.getDisplayed(0), "0");
+    assert.strictEqual(DoubleConverters.getDisplayed('.0123456789123456991'), "0.0123456789123456991");
   });
 
   it('#Test check correct', function () {
